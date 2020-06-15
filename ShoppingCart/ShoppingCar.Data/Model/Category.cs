17a7a6cf8736f2ebaedd399c831ad42294dfa4c7
Bryant Baldallaque <38927423+bbaldallaque@users.noreply.github.com>
2020-06-15 11:29:00 -0400

    Shopping Car Data

---
+++ ShoppingCart/ShoppingCar.Data/Model/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace ShoppingCar.Data.Model
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public ICollection<ProductCategory> Products { get; set; }
+    }
+}
---
+++ ShoppingCart/ShoppingCar.Data/Model/Product.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace ShoppingCar.Data.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Description { get; set; }
+
+        public string ProductImage { get; set; }
+
+        public ICollection<ProductCategory> Categories { get; set; }
+    }
+}
